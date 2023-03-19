d385f0d386b2ead663e91ff06f0d2dc639e34599
SimoniNR <nrsimoni@hotmail.com>
2023-03-19 12:03:25 +0000

    Buttons_Connect/Send

--- TTT/Assets/Scripts/LoginUI.cs
+++ TTT/Assets/Scripts/LoginUI.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private Button _loginButton;
+   private Button _sendButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+      _loginButton = transform.Find("Connect").GetComponent<Button>();
+      _loginButton.onClick.AddListener(Connect);
+
+      _sendButton = transform.Find("Send").GetComponent<Button>();
+      _sendButton.onClick.AddListener(Send);
+   }
+
+   private void Connect()
+   {
+      Debug.Log("Connecting to the Server!");
+     // NetworkClient.Instance.Connect();
+   }
+
+   private void Send()
+   {
+      Debug.Log("Sending message to the server!");
+   }
 }
