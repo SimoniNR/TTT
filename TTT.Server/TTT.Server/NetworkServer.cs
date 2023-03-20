f75132c723ebc5e1ed59072b9163db121a1237de
SimoniNR <nrsimoni@hotmail.com>
2023-03-20 15:01:36 +0000

    network server
    I started to implement the network server.
    as I found several problems with visual studio I decided to stop and  re-instal visual studio before follow ahead

---
+++ TTT.Server/TTT.Server/NetworkServer.cs
@@ -0,0 +1,69 @@
+﻿using LiteNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using LiteNetLib.Utils;
+
+namespace TTT.Server
+{
+    public class NetworkServer : INetEventListener
+    {
+        NetManager _netManager;
+        
+        public void Start()
+        {
+            _netManager = new NetManager(this);
+            {
+                DisconnectTimeout = 100000
+            };
+            _netManager.Start(9050);
+
+            Console.WriteLine("Server Listening on Port 9050");
+        }
+
+        public void PoolEvents()
+        {
+
+        }
+        
+        
+        public void OnConnectionRequest(ConnectionRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerConnected(NetPeer peer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
